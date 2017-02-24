8614622ab2a9f3e6071af7b29d33da4567092734
Kainoa Drost <kaid@beaverisland.k12.mi.us>
2017-02-24 16:06:37 -0500

    First working camera code and better code for xbox controller.

--- src/robot.py
+++ src/robot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
-    This is the most lit foundation to build robot code on
+    This is the most lit robot code for the most lit robot
 """
 
 import wpilib
+from xbox import XboxController
 # from robotpy_ext.common_drivers import navx
 
 class MyRobot(wpilib.IterativeRobot):
@@ -13,7 +14,10 @@ class MyRobot(wpilib.IterativeRobot):
         is used for initialization code.
         """
         # joystick 1 on the driver station
-        self.stick = wpilib.Joystick(0)
+        self.stick = XboxController(0)
+        
+        # start camera server
+        wpilib.CameraServer.launch('vision.py:main')
 
         # object that handles basic drive operations
         self.robot_drive = wpilib.RobotDrive(0, 1, 2, 3)
@@ -47,7 +51,7 @@ class MyRobot(wpilib.IterativeRobot):
         try:
             # Check if we've completed 100 loops (approximately 2 seconds)
             if self.auto_loop_counter < 100:
-                self.robot_drive.mecanumDrive_Cartesian(-0.1, 0, 0, 0) # Drive forwards
+                self.robot_drive.mecanumDrive_Cartesian(0, -0.1, 0, 0) # Drive forwards
                 self.auto_loop_counter += 1
             else:
                 self.robot_drive.mecanumDrive_Cartesian(0, 0, 0, 0)    #Stop robot
@@ -58,51 +62,47 @@ class MyRobot(wpilib.IterativeRobot):
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
         try:
-            xAxis = self.stick.getRawAxis(1) #Get value
-            xAxis = self.normalize(xAxis, 0.18) #Set deadzone
+            xAxis = self.stick.getLeftX() #Get joystick value
+            xAxis = self.normalize(xAxis, 0.1) #Set deadzone
             xAxis = self.joystickAdjust(xAxis, 0.5) #Adjust sensitivity
             
-            yAxis = self.stick.getRawAxis(0)
-            yAxis = self.normalize(yAxis, 0.18)
+            yAxis = self.stick.getLeftY()
+            yAxis = self.normalize(yAxis, 0.1)
             yAxis = self.joystickAdjust(yAxis, 0.5)
             
-            rotation = self.stick.getRawAxis(4)
-            rotation = self.normalize(rotation, 0.18)
+            rotation = self.stick.getRightX()
+            rotation = self.normalize(rotation, 0.1)
             rotation = self.joystickAdjust(rotation, 0.5)
             
 #             gyroAngle = self.gyro.getAngle()
             gyroAngle = 0
             self.robot_drive.mecanumDrive_Cartesian(xAxis, yAxis, rotation, gyroAngle)
-            
-            left_trig = self.stick.getRawAxis(2)
-            right_trig = self.stick.getRawAxis(3)
-#             right_trig = right_trig * -1
-
-            self.winch.set(right_trig * -1)
-            self.agitator.set(left_trig * -1)
  
-            if self.stick.getRawButton(1) is True:
-                self.loader.set(1)
+            if self.stick.getLeftBumper() is True:
+                self.winch.set(1)
             else:
-                self.loader.set(0)
+                self.winch.set(0)
                 
-            if self.stick.getRawButton(2) is True:
+            if self.stick.getLeftTrigger() is True:
                 self.intake.set(0.65)
             else:
                 self.intake.set(0)
-
-            if self.stick.getRawButton(4) is True:
-                self.stick.setRumble(0, 1)
-                self.stick.setRumble(1, 1)
-            else:
-                self.stick.setRumble(0, 0)
-                self.stick.setRumble(1, 0)
                 
-            if self.stick.getRawButton(5) is True:
+            if self.stick.getRightTrigger() is True:
                 self.shooter.set(0.70387)
+                self.agitator.set(1)
+                self.loader.set(1)
             else:
                 self.shooter.set(0)
+                self.agitator.set(0)
+                self.loader.set(0)
 
+            if self.stick.getButtonY() is True:
+#                 self.stick.setRumble(0, 1)
+                self.stick.rumble(1, 1)
+            else:
+                self.stick.rumble(0, 0)
+#                 self.stick.setRumble(1, 0)
         except:
             if not self.isFmsAttached():
                 raise
