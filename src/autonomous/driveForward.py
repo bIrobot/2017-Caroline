4b8eed31153e255749ba97cdd0383fda85c61b37
Kainoa Drost <kaid@beaverisland.k12.mi.us>
2017-03-05 22:29:30 -0500

    Fixed some values and comment.

--- src/autonomous/driveForward.py
+++ src/autonomous/driveForward.py
@@ -1,20 +1,52 @@
 from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
+from robotpy_ext.common_drivers.navx import AHRS
         
 class DriveForward(StatefulAutonomous):
     MODE_NAME = 'Drive Forward'
     DEFAULT = True
 
+    # These PID parameters are used on a real robot
+    kP = 0.03
+    kI = 0.00
+    kD = 0.00
+    kF = 0.00
+    
+    kToleranceDegrees = 1.0
     def initialize(self):
-        pass
+        
+        self.ahrs = AHRS.create_spi()
+         
+        turnController = self.PIDController(self.kP, self.kI, self.kD, self.kF, self.ahrs, output=self)
+        turnController.setInputRange(-180.0,  180.0)
+        turnController.setOutputRange(-1.0, 1.0)
+        turnController.setAbsoluteTolerance(self.kToleranceDegrees)
+        turnController.setContinuous(True)
+         
+        self.turnController = turnController
+        
+        self.addActuator("DriveSystem", "RotateController", turnController)
+        
+        self.ahrs.reset()
+        self.turnController.disable()
 
     @timed_state(duration=0.5, next_state='drive_forward', first=True)
     def drive_wait(self):
         self.robot_drive.mecanumDrive_Cartesian(0, 0, 0, 0) #Stop robot
 
-    @timed_state(duration=5, next_state='stop')
+    @timed_state(duration=4.3, next_state='stop')
     def drive_forward(self):
-        self.robot_drive.mecanumDrive_Cartesian(0, -0.5, 0, 0) #Drive forwards
-        
+        self.turnController.setSetpoint(0.0)
+        self.turnController.enable()
+        rotation = self.rotateToAngleRate
+        gyroAngle = self.ahrs.getAngle()
+        self.robot_drive.mecanumDrive_Cartesian(0, 0.25, rotation*-1, gyroAngle) #Drive forwards and straight (hopefully)
+#         print("NavX Gyro", self.ahrs.getYaw(), self.ahrs.getAngle())
     @state()
     def stop(self):
         self.robot_drive.mecanumDrive_Cartesian(0, 0, 0, 0) #Stop robot
+        
+    def pidWrite(self, output):
+        """This function is invoked periodically by the PID Controller,
+        based upon navX MXP yaw angle input and PID Coefficients.
+        """
+        self.rotateToAngleRate = output
